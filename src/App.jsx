1580bdd149021cb9125dc5494c77aef508b41714
Danyal494 <dk7167301@gmail.com>
2024-08-29 16:55:42 +0500

    mouse Bug

--- src/App.jsx
+++ src/App.jsx
@@ -12,34 +12,34 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const cursorDot = cursorDotRef.current;
-    const cursorOutline = cursorOutlineRef.current;
-
     const handleMouseMove = (e) => {
-      const posX = e.clientX;
-      const posY = e.clientY;
-
-      cursorDot.style.left = `${posX}px`;
-      cursorDot.style.top = `${posY}px`;
-
-      cursorOutline.style.left = `${posX}px`;
-      cursorOutline.style.top = `${posY}px`;
-
-      cursorOutline.animate(
-        {
-          left: `${posX}px`,
-          top: `${posY}px`,
-        },
-        { duration: 500, fill: "forwards" }
-      );
+      const cursorDot = cursorDotRef.current;
+      const cursorOutline = cursorOutlineRef.current;
+  
+      if (cursorDot && cursorOutline) {
+        const posX = e.clientX;
+        const posY = e.clientY;
+  
+        cursorDot.style.transform = `translate(${posX}px, ${posY}px)`;
+        cursorOutline.style.transform = `translate(${posX}px, ${posY}px)`;
+  
+        cursorOutline.animate(
+          {
+            transform: `translate(${posX}px, ${posY}px)`,
+          },
+          { duration: 500, fill: "forwards" }
+        );
+      }
     };
-
+  
     window.addEventListener("mousemove", handleMouseMove);
-
+  
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
     };
-  }, []);
+  }, [loading]); // Add `loading` to the dependency array
+  
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
@@ -50,33 +50,30 @@ function App() {
 
   return (
     <div className="Mpage">
-      {loading ? (<Loader/>) :(
+      {loading ? (
+        <Loader />
+      ) : (
         <div>
-
-        <div
-        ref={cursorDotRef}
-        className="cursor-dot w-[5px] h-[5px] bg-slate-800 fixed top-0 left-0 z-10 rounded-full translate-y-[-50%] translate-x-[-50%] pointer-events-none"
-        data-cursor-dot
-        ></div>
-        <div
-        ref={cursorOutlineRef}
-        className="cursor-outline translate-y-[-50%] translate-x-[-50%]  w-[30px] h-[30px] fixed top-0 left-0 z-10 pointer-events-none  border-2 rounded-[50%] justify-center"
-        data-cursor-outline
-      >
-       
-      </div>
-      <Navbutton />
-      <div className="navbar p-5 flex-wrap flex justify-evenly items-center">
-        <Sidebar />
-        <Home />
-        <Navbar />
-      </div>
+          <div
+            ref={cursorDotRef}
+            className="cursor-dot w-[5px] h-[5px] bg-slate-800 fixed top-0 left-0 z-10 rounded-full translate-y-[-50%] translate-x-[-50%] pointer-events-none"
+            data-cursor-dot
+          ></div>
+          <div
+            ref={cursorOutlineRef}
+            className="cursor-outline translate-y-[-50%] translate-x-[-50%] w-[30px] h-[30px] fixed top-0 left-0 z-10 pointer-events-none border-2 rounded-[50%] justify-center"
+            data-cursor-outline
+          ></div>
+          <Navbutton />
+          <div className="navbar p-5 flex-wrap flex justify-evenly items-center">
+            <Sidebar />
+            <Home />
+            <Navbar />
+          </div>
         </div>
-    )}
-      
+      )}
     </div>
   );
 }
 
 export default App;
-
